7b644ab403b2dd5b79f5b75e9a07f4c5d258fa11
mdmunir <misbahuldmunir@gmail.com>
2016-12-07 14:24:44 +0700

    fix error bind params

--- WorkerController.php
+++ WorkerController.php
@@ -2,14 +2,22 @@
 
 namespace dee\queue;
 
+use Yii;
+use yii\base\InlineAction;
+use yii\console\Exception;
+use yii\base\Controller;
+
 /**
  * Description of WorkerController
  *
  * @author Misbahul D Munir <misbahuldmunir@gmail.com>
  * @since 1.0
  */
-class WorkerController extends \yii\console\Controller
+class WorkerController extends Controller
 {
+    /**
+     * @var array
+     */
     private $_actionParams;
 
     /**
@@ -18,9 +26,35 @@ class WorkerController extends \yii\console\Controller
     public function bindActionParams($action, $params)
     {
         $this->_actionParams = $params;
-        return parent::bindActionParams($action, $params);
-    }
+        if ($action instanceof InlineAction) {
+            $method = new \ReflectionMethod($this, $action->actionMethod);
+        } else {
+            $method = new \ReflectionMethod($action, 'run');
+        }
 
+        $args = array_values($params);
+
+        $missing = [];
+        foreach ($method->getParameters() as $i => $param) {
+            if ($param->isArray() && isset($args[$i])) {
+                $args[$i] = preg_split('/\s*,\s*/', $args[$i]);
+            }
+            if (!isset($args[$i])) {
+                if ($param->isDefaultValueAvailable()) {
+                    $args[$i] = $param->getDefaultValue();
+                } else {
+                    $missing[] = $param->getName();
+                }
+            }
+        }
+
+        if (!empty($missing)) {
+            throw new Exception(Yii::t('yii', 'Missing required arguments: {params}', ['params' => implode(', ', $missing)]));
+        }
+
+        return $args;
+    }
+    
     /**
      * @return array
      */
